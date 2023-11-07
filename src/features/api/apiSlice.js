42ab75e4d1b3ad227486cd6178610dd4a98c50e5
seyha26 <seyhachheng26@email.com>
2023-11-07 16:06:54 +0700

    Redux Toolkit lesson06

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todo"],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/todos",
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
+      providesTags: ["Todo"],
+    }),
+    addTodo: builder.mutation({
+      query: (todo) => ({ url: "/todos", method: "POST", body: todo }),
+      invalidatesTags: ["Todo"],
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo,
+      }),
+      invalidatesTags: ["Todo"],
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({ url: `/todos/${id}`, method: "DELETE" }),
+      invalidatesTags: ["Todo"],
+    }),
+  }),
+});
+
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
